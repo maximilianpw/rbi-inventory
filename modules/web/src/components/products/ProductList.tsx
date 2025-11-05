91b42740cf5859e2a6799bb57e18550c3855f690
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-11-05 19:34:44 +0100

    feat: spinner

--- modules/web/src/components/products/ProductList.tsx
+++ modules/web/src/components/products/ProductList.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { CategoriesList } from '@/lib/data/components/categories'
 import { CategoryCard } from '../common/category/CategoryCard'
+import { Spinner } from '../ui/spinner'
 
 export function ProductList() {
   return (
     <CategoriesList
       renderItem={(cat) => <CategoryCard key={cat.id} category={cat} />}
+      fallback={<Spinner className="size-8" />}
     />
   )
 }
